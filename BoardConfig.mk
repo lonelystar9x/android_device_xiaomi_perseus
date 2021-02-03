2d2fff1a629ed9fc58c886a63ece5a3142ca2cfc
lonelystar9x <38067026+lonelystar9x@users.noreply.github.com>
2021-02-04 05:39:24 +0700

    init Crdroid

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,7 +39,16 @@ TARGET_USES_NQ_NFC := true
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
+ifeq ($(BOARD_AVB_ENABLE), true)
+   BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
+   BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
+   BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA2048
+   BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
+   BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
+endif
+
+# SELinux
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Inherit from the proprietary version
 -include vendor/xiaomi/perseus/BoardConfigVendor.mk
